f3fed864244ff1e6432888d2fd74ba83996109fa
alfaxad1 <alfaxadder@gmail.com>
2026-02-27 02:44:16 +0300

    allowed all origins

--- src/main/java/com/example/acacia/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/example/acacia/SecurityConfig/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(allowedOrigins);
+        cfg.setAllowedOriginPatterns(List.of("*"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "Content-Disposition", "X-BranchId"));
         cfg.setAllowCredentials(true);
