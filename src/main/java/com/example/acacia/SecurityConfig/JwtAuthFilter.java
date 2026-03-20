7ca6ddc6c100b07e3209202a5ad592bb13ae3d8d
alfaxad1 <alfaxadder@gmail.com>
2026-03-20 11:07:47 +0300

    improved login

--- src/main/java/com/example/acacia/SecurityConfig/JwtAuthFilter.java
+++ src/main/java/com/example/acacia/SecurityConfig/JwtAuthFilter.java
@@ -3,6 +3,7 @@ package com.example.acacia.SecurityConfig;
 
 import com.example.acacia.service.MemberDetailsService;
 import com.example.acacia.service.MemberService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,42 +34,51 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain)
             throws ServletException, IOException {
+                try {
+                    final String authHeader = request.getHeader("Authorization");
 
-            final String authHeader = request.getHeader("Authorization");
+                    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                        filterChain.doFilter(request, response);
+                        return;
+                    }
 
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                filterChain.doFilter(request, response);
-                return;
-            }
+                    String jwtToken = authHeader.substring(7);
+                    String username = jwtUtils.extractUsername(jwtToken);
 
-            String jwtToken = authHeader.substring(7);
-            String username = jwtUtils.extractUsername(jwtToken);
+                    if (username != null &&
+                            SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            if (username != null &&
-                    SecurityContextHolder.getContext().getAuthentication() == null) {
-
-                UserDetails userDetails = memberDetailsService.loadUserByUsername(username);
+                        UserDetails userDetails = memberDetailsService.loadUserByUsername(username);
 
 
-                if (jwtUtils.isTokenValid(jwtToken, userDetails)) {
+                        if (jwtUtils.isTokenValid(jwtToken, userDetails)) {
 
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(
-                                    userDetails,
-                                    null,
-                                    userDetails.getAuthorities()
-                            );
+                            UsernamePasswordAuthenticationToken authToken =
+                                    new UsernamePasswordAuthenticationToken(
+                                            userDetails,
+                                            null,
+                                            userDetails.getAuthorities()
+                                    );
 
-                    authToken.setDetails(new WebAuthenticationDetailsSource()
-                            .buildDetails(request));
+                            authToken.setDetails(new WebAuthenticationDetailsSource()
+                                    .buildDetails(request));
 
-                    SecurityContextHolder.getContext()
-                            .setAuthentication(authToken);
+                            SecurityContextHolder.getContext()
+                                    .setAuthentication(authToken);
 
+                        }
+                    }
+
+                    filterChain.doFilter(request, response);
+                } catch (ExpiredJwtException e) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Token expired");
+                    return; // Stop the filter chain here
+                } catch (Exception e) {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    return;
                 }
-            }
 
-            filterChain.doFilter(request, response);
 
     }
 }
